a28dcde57bb613305aa4efa4e98f354ae758f4ed
Prashant Karmakar <webster15july@gmail.com>
2014-03-02 13:46:54 +0530

    updated my email in license :P

--- sud.c
+++ sud.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014, Prashant Karmakar, pk80@live.com
+Copyright (c) 2014, Prashant Karmakar, webster15july@gmail.com
 
  Permission to use, copy, modify, and/or distribute this software for
  any purpose with or without fee is hereby granted, provided that
