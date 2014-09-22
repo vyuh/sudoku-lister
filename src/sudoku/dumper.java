8f1ed3f595072438551cdf73a01eecab78fa2e36
Prashant Karmakar <webster15july@gmail.com>
2014-09-22 16:27:23 +0530

    attempts on enabling dump feature

--- src/sudoku/dumper.java
+++ src/sudoku/dumper.java
@@ -1,40 +1,44 @@
-typedef struct {
-    s00d *s;
-    int p;
-    int v;
-} state;
-struct {
-    char *buf;
-    state *stack;
-    int top;
-    int pos;
-} d;
+package sudoku;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+class state {
+    s00d s;
+    byte p;
+    byte v;
+}
+class dumper {
+    StringBuilder buf;
+    state[] stack;
+    byte top;
+    byte pos;
+    SignalHandler rq;
+    void burn(){
+        stack = null;
+        buf = null;
+    }
+    dumper(){
+        rq = new SignalHandler(){
+            public void handle(Signal sig){
+                System.err.println("dump requested");
+                buf = new StringBuilder(8280); //TODO count recursion depth and use that to decide capacity!
+                //TODO do i neet to set zeroes? or better use a byte arraylist?
+            }
+        };
+        Signal.handle(new Signal("INT", rq);
+    } 
+}
 int dmp(s00d *s, int p, int v){
     char *eye;
     b16 *sun;
     int l;
 
-    for(l=0, eye=d.buf+d.pos, sun=s->i_v; l<405; sun++, eye+=5) l+=sprintf(eye, "%04x ", (int)*sun);
+    for(l=0, eye=d.buf+d.pos, sun=s->i_v; l<405; sun++, eye+=5) l+=sprintf(eye, "%04x ", (int)*sun); //TODO java formatted output
 
     l+=sprintf(eye, "%02x ", (int)s->left); eye+=3;
     l+=sprintf(eye, "%02x ", p); eye+=3;
     l+=sprintf(eye, "%02x\n", v);
 
     return l;//the characters written d.pos to be incremented outside
-}
-int burn(state *n){
-    for(;n>=d.stack;n--) free(n->s);
-    free(d.stack);
-    d.stack=0;
-    free(d.buf);
-    d.buf=0;
-    return 1;
-}
-#include <string.h>
-void rq(int sig){
-    fputs("\ndump requested\n", stderr);
-    if(!(d.buf=malloc(81*414))) die("RAM denied");
-    memset(d.buf, 0, 81*414);
 }
 int rd(){
     FILE *dump;
@@ -59,7 +63,7 @@ int rd(){
         now=d.stack+d.top;
         if(!(now->s=(s00d *)malloc(sizeof(s00d)))) die("RAM denied\n");
         for(i=0,this=line,eye=now->s->i_v;i<81;i++,eye++){
-            *eye=(b16)strtoul(this,&nxt,16);
+            *eye=(b16)strtoul(this,&nxt,16);//TODO java formatted input
             if((this+5!=nxt)&&(i!=0||(this+4!=nxt))) return burn(now);
             this=nxt; 
         }
@@ -71,7 +75,7 @@ int rd(){
 	this=nxt;
 	now->v=(b8)strtoul(this,&nxt,16);
 	if(this+3!=nxt) return burn(now);
-        line=strtok(0,"\r\n");
+        line=strtok(0,"\r\n");//TODO java tokenization
 	d.top++;
 	depth--;
     }
